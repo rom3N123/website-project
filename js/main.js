cc57f200412a20b1b67091ec9275f0c39cdd597f
Roman <kseexy@mail.ru>
2021-03-11 17:42:46 +0300

    FINALY DONE

--- js/main.js
+++ js/main.js
@@ -12,4 +12,14 @@ $(function () {
 	$('.header__burger').on('click', (event) => {
 		$('.header__burger,.menu').toggleClass('active');
 	});
+
+
+
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(_href).offset().top + "px"
+		});
+		return false;
+	});
 });
