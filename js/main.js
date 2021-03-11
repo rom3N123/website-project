8af11ea948bdaa4ac1ee33b6dde918e543d24ba8
Roman <kseexy@mail.ru>
2021-03-11 17:15:21 +0300

    Done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function () {
-	$('.reviews__slider-inner').slick();
+	$('.reviews__slider-inner').slick({
+		responsive: [{
+			breakpoint: 1280,
+			settings: {
+				arrows: false,
+				dots: true
+			}
+		}]
+	});
 
 	$('.header__burger').on('click', (event) => {
 		$('.header__burger,.menu').toggleClass('active');
